47039a917d28c208564b88ca28ae19b6f584f7e3
MKluna <matiaslunastyles@gmail.com>
2022-08-13 02:24:36 -0300

    Update | Cards Routes

--- routes/cards.js
+++ routes/cards.js
@@ -1,38 +1,48 @@
 const express = require("express");
 const router = express.Router();
+const authMiddleware = require("../middleware/checkSession");
+const checkRol = require("../middleware/checkRoles");
+
 const {
   getCards,
   getCardById,
   createCard,
   updateCard,
-  deleteCard
+  deleteCard,
 } = require("../controllers/cards");
+
 const {
   validatorGetCard,
-  validatorCreateCard
+  validatorCreateCard,
 } = require("../validators/cards");
 
 router.get("/", getCards);
+
 router.get("/:id", validatorGetCard, getCardById);
+
 router.post(
   "/",
-  // authMiddleware,
-  // checkRol(["user", "admin"]),
+  authMiddleware,
+  checkRol(["admin"]),
   validatorCreateCard,
   createCard
 );
+
 router.put(
   "/:id",
-  // authMiddleware,
+  authMiddleware,
+  checkRol(["admin"]),
   validatorGetCard,
   validatorCreateCard,
   updateCard
 );
-router.delete("/:id",
-  // authMiddleware,
-  // checkRol(["user", "admin"]),
+
+router.delete(
+  "/:id",
+  authMiddleware,
+  checkRol(["admin"]),
   validatorGetCard,
   deleteCard
-)
+);
 
 module.exports = router;
