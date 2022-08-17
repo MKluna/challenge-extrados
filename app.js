a1d1c27f673381f6ce4f15f55630d9dcf1278ab1
MKluna <matiaslunastyles@gmail.com>
2022-08-17 10:51:00 -0300

    UPDATE | APP AND MONGO CONNECTION

--- app.js
+++ app.js
@@ -5,13 +5,19 @@ const dbConnect = require("./config/mongo");
 const app = express();
 const port = process.env.PORT || 3000;
 
+const NODE_ENV = process.env.NODE_ENV || 'development'
+
 app.use(cors());
 app.use(express.json());
 
 app.use("/api", require("./routes"));
 
-app.listen(port, () => {
-  console.log(`Application ready in http://localhost:${port}/api`);
-});
+if (NODE_ENV !== 'test') {
+  app.listen(port, () => {
+    console.log(`Application ready in http://localhost:${port}/api`);
+  });
+}
 
 dbConnect();
+
+module.exports = app
--- config/mongo.js
+++ config/mongo.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
+const NODE_ENV = process.env.NODE_ENV
 
 const dbConnect = () => {
-  const db_uri = process.env.DB_URI;
+  const db_uri = (NODE_ENV === 'test') ? process.env.DB_URI_TEST : process.env.DB_URI;
   mongoose.connect(
     db_uri,
     { useNewUrlParser: true, useUnifiedTopology: true },
